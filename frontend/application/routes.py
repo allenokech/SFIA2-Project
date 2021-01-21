b4891a1ba5201a5173fa7193475ab05426db1480
allenokech <user@sfia2.europe-west2-c.c.leafy-catbird-296317.internal>
2021-01-21 12:36:53 +0000

    Application working

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -5,12 +5,14 @@ from application import app, db
 
 @app.route('/', methods=['GET', 'POST'])
 def gen():
-    race_response = requests.get("http://service2:5000/race")
-    player_class_response = requests.get("http://service3:5000/player_class")
-    build_response = requests.post("http://service4:5000/build", 
-    json={"race" : race_response.text, "player_class" : player_class_response.text})
+    race_response = requests.get("http://character-gen_service2:5000/race")
+    player_class_response = requests.get("http://character-gen_service3:5000/player_class")
+    build_response = requests.post("http://character-gen_service4:5000/build", data = player_class_response.text)
     
     new_build = Character(race = race_response.text, player_class = player_class_response.text, build = build_response.text)
     db.session.add(new_build)
     db.session.commit()
-    return render_template('index.html', race=race_response.text, player_class=player_class_response.text, build=build_response.text)
+
+    previous = Character.query.first()
+    
+    return render_template('index.html', race=race_response.text, player_class=player_class_response.text, build=build_response.text, previous=previous)
