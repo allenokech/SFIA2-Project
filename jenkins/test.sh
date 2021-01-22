fff1967bf70964e4a0146d2b3a22e7ed646eebca
allenokech <user@sfia2.europe-west2-c.c.leafy-catbird-296317.internal>
2021-01-22 16:30:27 +0000

    Changes to test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -10,35 +10,19 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
 
 # Test Service 2
 cd service2
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
 
 # Test Service 3
 cd service3
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
 
 # Test Service 4
 cd service4
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
