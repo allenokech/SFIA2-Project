9fea975231e9207de861b9034699b592d9970490
allenokech <user@sfia2.europe-west2-c.c.leafy-catbird-296317.internal>
2021-01-23 19:36:37 +0000

    Deploy script edit

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,5 @@
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml character-gen
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml character-gen-stack
 EOF
